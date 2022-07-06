cdeedc00d80b8ec17bbc62dcee30eea069b74086
Peter Donald <peter@realityforge.org>
2022-07-06 12:00:02 +1000

    Add LICENSE headers where appropriate

--- tests/braincheck_backtrace_test.cpp
+++ tests/braincheck_backtrace_test.cpp
@@ -1,3 +1,17 @@
+/*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 #include "test_support.inc"
 
 using Catch::Matchers::ContainsSubstring;
@@ -35,7 +49,7 @@ TEST_CASE("Backtrace includes file, line and function as well as stack")
 #define EXPECTED_FUNC_NAME "func1"
 #endif
 
-    REQUIRE_THAT(message, StartsWith(__FILE__ ":8: " EXPECTED_FUNC_NAME));
+    REQUIRE_THAT(message, StartsWith(__FILE__ ":22: " EXPECTED_FUNC_NAME));
 #ifdef DEBUG
     REQUIRE_THAT(message, ContainsSubstring("func2"));
     REQUIRE_THAT(message, ContainsSubstring("func3"));
--- tests/c_compile_test.c
+++ tests/c_compile_test.c
@@ -11,6 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #pragma clang diagnostic ignored "-Wvariadic-macros"
 #pragma clang diagnostic ignored "-Wgnu-zero-variadic-macro-arguments"
 #define BRAINCHECK_PRINTF(args...) printf(args)
